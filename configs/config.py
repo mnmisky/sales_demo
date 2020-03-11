fe3991d6199a21ca71f0b481f840b8b720be7d45
mnmisky <mnmisky@gmail.com>
2020-03-11 19:18:07 +1100

    dep

--- configs/config.py
+++ configs/config.py
@@ -3,8 +3,8 @@ class Config(object):
 
 class Development(Config):
     DEBUG=True
-    # SQLALCHEMY_DATABASE_URI='postgresql://postgres:Miskitoo.1998@127.0.0.1:5432/sales_demo'
-    SQLALCHEMY_DATABASE_URI='postgres://gygwqrdwerdekx:3b24d6681e35a1c68211f7026e627708f43e92cb06f914303865b1636d4db1f7@ec2-18-210-51-239.compute-1.amazonaws.com:5432/de723tjimc0c7b'
+      SQLALCHEMY_DATABASE_URI='postgresql://postgres:Miskitoo.1998@127.0.0.1:5432/sales_demo'
+    # SQLALCHEMY_DATABASE_URI='postgres://gygwqrdwerdekx:3b24d6681e35a1c68211f7026e627708f43e92cb06f914303865b1636d4db1f7@ec2-18-210-51-239.compute-1.amazonaws.com:5432/de723tjimc0c7b'
     SECRET_KEY='MISKY'
 
 class Production (Config):
