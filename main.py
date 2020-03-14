8d591505f49c6e96a1f6bdaf401c24e47b9020ce
mnmisky <mnmisky@gmail.com>
2020-03-15 04:32:19 +1100

    kkk

--- main.py
+++ main.py
@@ -281,6 +281,10 @@ def online():
 def contact():
    return render_template('contact.html')
 
+@app.route ("/pricing")
+def pricing():
+   return render_template('pricing.html')
+
 
 @app.route ("/creative")
 def creative():
